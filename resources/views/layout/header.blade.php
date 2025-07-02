54f5099c9393a973bb2f0bcadfea588c251e6b46
szansari <salmanzafaransari>
2025-07-02 13:42:37 +0530

    added layout module

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,239 @@
+<section class="topbar">
+    <div class="container-fluid">
+        <div class="topbar__wrapper">
+            <ul class="list-unstyled topbar__list">
+                <li>
+                    <span class="fas fa-envelope"></span>
+                    <a href="mailto:needhelp@company.com">needhelp@company.com</a>
+                </li>
+                <li>
+                    <span class="fas fa-map-marker"></span>
+                    88 Broklyn Golden Street. New York
+                </li>
+            </ul><!-- /.icon-box -->
+            <ul class="list-unstyled topbar__menu">
+                <li><a href="faq.html">Help</a></li>
+                <li><a href="about.html">Support</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul><!-- /.list-menu -->
+            <div class="topbar__social">
+                <a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a>
+                <a href="https://www.pinterest.com/"><i class="fab fa-pinterest-p"></i></a>
+                <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+            </div><!-- /.social-links -->
+        </div>
+    </div>
+</section><!-- /.topbar-header -->
+<header class="main-header">
+    <nav class="main-menu">
+        <div class="container-fluid">
+            <div class="main-menu__logo">
+                <a href="index.html">
+                    <img src="{{ asset('assets/images/logo-light.png') }}" width="96" height="34" alt="Ogency">
+                </a>
+            </div><!-- /.main-menu__logo -->
+            <div class="main-menu__nav">
+                <ul class="main-menu__list">
+                    <li class="dropdown megamenu">
+                        <a href="index.html">Home</a>
+                        <ul>
+                            <li>
+                                <section class="home-showcase">
+                                    <div class="container">
+                                        <div class="home-showcase__inner">
+                                            <div class="row">
+                                                <div class="col-lg-3">
+                                                    <div class="home-showcase__item">
+                                                        <div class="home-showcase__image">
+                                                            <img src="{{ asset('assets/images/home-showcase/home-showcase-1-1.jpg') }}" alt="">
+                                                            <div class="home-showcase__buttons">
+                                                                <a href="index.html" class="nisoz-btn home-showcase__buttons__item">
+                                                                    <span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span>
+                                                                    <span class="nisoz-btn__text">Multi Page</span>
+                                                                </a>
+                                                                <a href="index-one-page.html" class="nisoz-btn home-showcase__buttons__item">
+                                                                    <span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span>
+                                                                    <span class="nisoz-btn__text">One Page</span>
+                                                                </a>
+                                                            </div>
+                                                            <!-- /.home-showcase__buttons -->
+                                                        </div><!-- /.home-showcase__image -->
+                                                        <h3 class="home-showcase__title">Homepage 01</h3><!-- /.home-showcase__title -->
+                                                    </div><!-- /.home-showcase__item -->
+                                                </div><!-- /.col-lg-3 -->
+                                                <div class="col-lg-3">
+                                                    <div class="home-showcase__item">
+                                                        <div class="home-showcase__image">
+                                                            <img src="{{ asset('assets/images/home-showcase/home-showcase-1-2.jpg') }}" alt="">
+                                                            <div class="home-showcase__buttons">
+                                                                <a href="index-2.html" class="nisoz-btn home-showcase__buttons__item">
+                                                                    <span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span>
+                                                                    <span class="nisoz-btn__text">Multi Page</span>
+                                                                </a>
+                                                                <a href="index-2-one-page.html" class="nisoz-btn home-showcase__buttons__item">
+                                                                    <span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span>
+                                                                    <span class="nisoz-btn__text">One Page</span>
+                                                                </a>
+                                                            </div>
+                                                            <!-- /.home-showcase__buttons -->
+                                                        </div><!-- /.home-showcase__image -->
+                                                        <h3 class="home-showcase__title">Homepage 02</h3><!-- /.home-showcase__title -->
+                                                    </div><!-- /.home-showcase__item -->
+                                                </div><!-- /.col-lg-3 -->
+                                                <div class="col-lg-3">
+                                                    <div class="home-showcase__item">
+                                                        <div class="home-showcase__image">
+                                                            <img src="{{ asset('assets/images/home-showcase/home-showcase-1-3.jpg') }}" alt="">
+                                                            <div class="home-showcase__buttons">
+                                                                <a href="index-3.html" class="nisoz-btn home-showcase__buttons__item">
+                                                                    <span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span>
+                                                                    <span class="nisoz-btn__text">Multi Page</span>
+                                                                </a>
+                                                                <a href="index-3-one-page.html" class="nisoz-btn home-showcase__buttons__item">
+                                                                    <span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span>
+                                                                    <span class="nisoz-btn__text">One Page</span>
+                                                                </a>
+                                                            </div>
+                                                            <!-- /.home-showcase__buttons -->
+                                                        </div><!-- /.home-showcase__image -->
+                                                        <h3 class="home-showcase__title">Homepage 03</h3><!-- /.home-showcase__title -->
+                                                    </div><!-- /.home-showcase__item -->
+                                                </div><!-- /.col-lg-3 -->
+                                                <div class="col-lg-3">
+                                                    <div class="home-showcase__item">
+                                                        <div class="home-showcase__image">
+                                                            <img src="{{ asset('assets/images/home-showcase/home-showcase-1-4.jpg') }}      " alt="">
+                                                            <div class="home-showcase__buttons">
+                                                                <a href="index-dark.html" class="nisoz-btn home-showcase__buttons__item">
+                                                                    <span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span>
+                                                                    <span class="nisoz-btn__text">View Page</span>
+                                                                </a>
+                                                            </div>
+                                                            <!-- /.home-showcase__buttons -->
+                                                        </div><!-- /.home-showcase__image -->
+                                                        <h3 class="home-showcase__title">Homepage Dark</h3><!-- /.home-showcase__title -->
+                                                    </div><!-- /.home-showcase__item -->
+                                                </div><!-- /.col-lg-3 -->
+                                            </div><!-- /.row -->
+                                        </div><!-- /.home-showcase__inner -->
+                                    </div><!-- /.container -->
+                                </section>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#">Pages</a>
+                        <ul>
+                            <li><a href="about.html">About</a></li>
+                            <li><a href="team.html">Our Team</a></li>
+                            <li><a href="team-carousel.html">Team carousel</a></li>
+                            <li><a href="team-details.html">Team detail</a></li>
+                            <li><a href="gallery.html">Gallery</a></li>
+                            <li><a href="gallery-carousel.html">Gallery carousel</a></li>
+                            <li><a href="pricing.html">Pricing</a></li>
+                            <li><a href="faq.html">FAQs</a></li>
+                            <li><a href="login.html">Login</a></li>
+                            <li><a href="404.html">404 error</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#">Services</a>
+                        <ul>
+                            <li><a href="services.html">Services</a></li>
+                            <li><a href="services-carousel.html">Services carousel 01</a></li>
+                            <li><a href="services-carousel-2.html">Services carousel 02</a></li>
+                            <li><a href="website-designing.html">Website Designing</a></li>
+                            <li><a href="website-development.html">Website Development</a></li>
+                            <li><a href="mobile-application.html">Mobile Application</a></li>
+                            <li><a href="digital-marketing.html">Digital Marketing</a></li>
+                            <li><a href="social-marketing.html">Social Marketing</a></li>
+                            <li><a href="seo-content.html">SEO & Content Writing</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#">Portfolio</a>
+                        <ul>
+                            <li class="dropdown">
+                                <a href="#">Portfolio Grid</a>
+                                <ul class="sub-menu">
+                                    <li><a href="portfolio-two-col.html">2 Columns</a></li>
+                                    <li><a href="portfolio.html">3 Columns</a></li>
+                                    <li><a href="portfolio-four-col.html">4 Columns</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="portfolio-carousel.html">Portfolio carousel</a></li>
+                            <li><a href="portfolio-masonry.html">Portfolio masonry</a></li>
+                            <li><a href="portfolio-details.html">Portfolio details</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#">Shop</a>
+                        <ul class="sub-menu">
+                            <li class="dropdown">
+                                <a href="#">Products</a>
+                                <ul class="sub-menu">
+                                    <li><a href="products.html">No sidebar</a></li>
+                                    <li><a href="products-left.html">Left sidebar</a></li>
+                                    <li><a href="products-right.html">Right sidebar</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="products-carousel.html">Products carousel</a></li>
+                            <li><a href="product-details.html">Product details</a></li>
+                            <li><a href="cart.html">Cart</a></li>
+                            <li><a href="checkout.html">Checkout</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#">Blog</a>
+                        <ul class="sub-menu">
+                            <li class="dropdown">
+                                <a href="#">Blog grid</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog-grid.html">No sidebar</a></li>
+                                    <li><a href="blog-grid-left.html">Left sidebar</a></li>
+                                    <li><a href="blog-grid-right.html">Right sidebar</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#">Blog list</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog-list.html">No sidebar</a></li>
+                                    <li><a href="blog-list-left.html">Left sidebar</a></li>
+                                    <li><a href="blog-list-right.html">Right sidebar</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="blog-carousel.html">Blog carousel</a></li>
+                            <li class="dropdown">
+                                <a href="#">Blog details</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog-details.html">No sidebar</a></li>
+                                    <li><a href="blog-details-left.html">Left sidebar</a></li>
+                                    <li><a href="blog-details-right.html">Right sidebar</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div><!-- /.main-menu__nav -->
+            <div class="main-menu__right">
+                <a href="#" class="main-menu__toggler mobile-nav__toggler">
+                    <i class="fa fa-bars"></i>
+                </a><!-- /.mobile menu btn -->
+                <a href="tel:+9236809850" class="main-menu__phone">
+                    <i class="icon-telephone"></i>
+                    +92 (3680) - 9850
+                </a><!-- /.phone-number -->
+                <a href="#" class="main-menu__search search-toggler">
+                    <i class="icon-magnifying-glass"></i>
+                </a><!-- /.search btn -->
+                <a href="cart.html" class="main-menu__cart cart-toggler">
+                    <i class="icon-shopping-cart"></i>
+                    <span class="main-menu__cart__count">0</span>
+                </a><!-- /.cart btn -->
+            </div><!-- /.main-menu__right -->
+        </div><!-- /.container -->
+    </nav>
+    <!-- /.main-menu -->
+</header><!-- /.main-header -->
