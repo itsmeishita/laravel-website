8ba1259d079a6b56ae62f6210739308775828ffe
szansari <salmanzafaransari>
2025-07-08 09:07:14 +0530

    lattest pull

---
+++ storage/framework/views/d1fac0bca16fb1105453a03fade3a767dcce9836.php
@@ -0,0 +1,129 @@
+
+<?php $__env->startSection('title','ramadan api documentation'); ?>
+
+<?php $__env->startSection('content'); ?>
+
+<div class="stricky-header stricked-menu main-menu">
+    <div class="sticky-header__content"></div><!-- /.sticky-header__content -->
+</div><!-- /.stricky-header -->
+<section class="page-header">
+    <div class="page-header__bg"></div><!-- /.page-header__bg -->
+    <div class="page-header__shape1"></div><!-- /.page-header__shape1 -->
+    <div class="page-header__shape2"></div><!-- /.page-header__shape1 -->
+    <div class="page-header__shape3 wow slideInRight" data-wow-delay="300ms"></div><!-- /.page-header__shape3 -->
+    <div class="container">
+        <ul class="page-header__breadcrumb list-unstyled">
+            <li><a href="index.html">Home</a></li>
+            <li><span>tool</span></li>
+        </ul><!-- /.page-breadcrumb list-unstyled -->
+        <h2 class="page-header__title">ramadan-api-documentation</h2><!-- /.page-title -->
+    </div><!-- /.container -->
+</section><!-- /.page-header -->
+<section>
+    <div class="container">
+        <h3 class="page-header__title" style="color: #333; text-align: center;">Ramadan API Documentation</h3>
+    </div>
+    <!-- Accordian Start -->
+    <section class="accrodion-one faq-page">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12 col-lg-12 wow fadeInRight" data-wow-delay="300ms">
+                    <div class="accrodion-one__wrapper nisoz-accrodion" data-grp-name="nisoz-accrodion">
+                        <div class="accrodion">
+                            <div class="accrodion-title">
+                                <h4>Get All Ramadan Data</h4>
+                            </div><!-- /.accordian-title -->
+                            <div class="accrodion-content">
+                                <div class="inner">
+                                    <p>API URL https://chikuweb.in/api/ramadan/all
+
+                                        Method: GET
+
+                                        Example Response:
+
+                                        {"ramadan_2025": [{"date": "2025-03-02" , "sehri" : "05:06 AM" , "iftar" : "06:02 PM" }, ...]}
+                                        Explanation: This endpoint returns the Sehri and Iftar times for all days of Ramadan for the specified year.</p>
+                                </div><!-- /.accordian-content -->
+                            </div>
+                        </div><!-- /.accordian-item -->
+                        <div class="accrodion active">
+                            <div class="accrodion-title">
+                                <h4>What are the different types of marketing solutions?</h4>
+                            </div><!-- /.accordian-title -->
+                            <div class="accrodion-content">
+                                <div class="inner">
+                                    <p>There are many variations of passages of available but the have suffered
+                                        alteration in that some form by injected randomised words which don’t
+                                        look even as slightly believable now.</p>
+                                </div><!-- /.accordian-content -->
+                            </div>
+                        </div><!-- /.accordian-item -->
+                        <div class="accrodion">
+                            <div class="accrodion-title">
+                                <h4>How often should i work on the digital marketing?</h4>
+                            </div><!-- /.accordian-title -->
+                            <div class="accrodion-content">
+                                <div class="inner">
+                                    <p>There are many variations of passages of available but the have suffered
+                                        alteration in that some form by injected randomised words which don’t
+                                        look even as slightly believable now.</p>
+                                </div><!-- /.accordian-content -->
+                            </div>
+                        </div><!-- /.accordian-item -->
+                        <div class="accrodion">
+                            <div class="accrodion-title">
+                                <h4>Are social media good for the business growth?</h4>
+                            </div><!-- /.accordian-title -->
+                            <div class="accrodion-content">
+                                <div class="inner">
+                                    <p>There are many variations of passages of available but the have suffered
+                                        alteration in that some form by injected randomised words which don’t
+                                        look even as slightly believable now.</p>
+                                </div><!-- /.accordian-content -->
+                            </div>
+                        </div><!-- /.accordian-item -->
+                        <div class="accrodion">
+                            <div class="accrodion-title">
+                                <h4>What activities are done in the development?</h4>
+                            </div><!-- /.accordian-title -->
+                            <div class="accrodion-content">
+                                <div class="inner">
+                                    <p>There are many variations of passages of available but the have suffered
+                                        alteration in that some form by injected randomised words which don’t
+                                        look even as slightly believable now.</p>
+                                </div><!-- /.accordian-content -->
+                            </div>
+                        </div><!-- /.accordian-item -->
+                        <div class="accrodion">
+                            <div class="accrodion-title">
+                                <h4>What is the most important thing in a designing?</h4>
+                            </div><!-- /.accordian-title -->
+                            <div class="accrodion-content">
+                                <div class="inner">
+                                    <p>There are many variations of passages of available but the have suffered
+                                        alteration in that some form by injected randomised words which don’t
+                                        look even as slightly believable now.</p>
+                                </div><!-- /.accordian-content -->
+                            </div>
+                        </div><!-- /.accordian-item -->
+                        <div class="accrodion">
+                            <div class="accrodion-title">
+                                <h4>What is the purpose of a UI / UX?</h4>
+                            </div><!-- /.accordian-title -->
+                            <div class="accrodion-content">
+                                <div class="inner">
+                                    <p>There are many variations of passages of available but the have suffered
+                                        alteration in that some form by injected randomised words which don’t
+                                        look even as slightly believable now.</p>
+                                </div><!-- /.accordian-content -->
+                            </div>
+                        </div><!-- /.accordian-item -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <?php $__env->stopSection(); ?>
+<?php echo $__env->make('default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\dell\Desktop\html_Tamplate\laravel-projects\chiku-redesign\resources\views/ramadan-api-documentation.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/edb6328d097ea0acee64638b70595398b79a804d.php
@@ -0,0 +1,124 @@
+
+<?php $__env->startSection('title','Contact Us'); ?>
+
+<?php $__env->startSection('style'); ?>
+<link rel="stylesheet" href="assets/vendors/bootstrap-select/bootstrap-select.min.css" />
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+
+<div class="stricky-header stricked-menu main-menu">
+    <div class="sticky-header__content"></div><!-- /.sticky-header__content -->
+</div><!-- /.stricky-header -->
+<section class="page-header">
+    <div class="page-header__bg"></div><!-- /.page-header__bg -->
+    <div class="page-header__shape1"></div><!-- /.page-header__shape1 -->
+    <div class="page-header__shape2"></div><!-- /.page-header__shape1 -->
+    <div class="page-header__shape3 wow slideInRight" data-wow-delay="300ms"></div><!-- /.page-header__shape3 -->
+    <div class="container">
+        <ul class="page-header__breadcrumb list-unstyled">
+            <li><a href="index.html">Home</a></li>
+            <li><span>Contact</span></li>
+        </ul><!-- /.page-breadcrumb list-unstyled -->
+        <h2 class="page-header__title">Contact</h2><!-- /.page-title -->
+    </div><!-- /.container -->
+</section><!-- /.page-header -->
+<!--Google Map Start-->
+<section class="google-map google-map-two">
+    <div class="container">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3210.5207862656416!2d82.58781030794823!3d25.415259434427515!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x398fdf508f0d0b77%3A0x4350490890b4b83!2sChiku%20Web!5e0!3m2!1sen!2sin!4v1731558356480!5m2!1sen!2sin" class="google-map__one" allowfullscreen></iframe>
+    </div>
+</section>
+<!--Google Map End-->
+<!--Contact Info Start-->
+<section class="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-12 wow fadeInLeft" data-wow-delay="200ms">
+                <div class="faq-page__help">
+                    <div class="faq-page__help__icon"><span class="icon-phone-call-1"></span></div>
+                    <h3 class="faq-page__help__title">Do you still<br> have questions?</h3>
+                    <p class="faq-page__help__text">Call Anytime<a href="tel:+9288009850">+92 (8800) - 9850</a></p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="contact__box">
+                    <div class="contact__box__icon"><span class="icon-mailbox"></span></div>
+                    <h5 class="contact__box__title">Write email</h5>
+                    <p class="contact__box__text"><a href="mailto:needhelp@company.com">needhelp@company.com</a></p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="contact__box contact__box-two">
+                    <div class="contact__box__icon"><span class="icon-maps-and-flags"></span></div>
+                    <h5 class="contact__box__title">Visit office</h5>
+                    <p class="contact__box__text">24 Valentin, Street Road New York</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Contact Info End-->
+<!-- Contact Start -->
+<section class="contact-one contact-page">
+    <div class="container wow fadeInUp" data-wow-delay="300ms">
+        <div class="section-title text-center">
+            <div class="section-title__triangle">
+                <span class="section-title__triangle-left"></span>
+                <span class="section-title__triangle-right"></span>
+            </div>
+            <h5 class="section-title__tagline">contact with us</h5>
+            <h2 class="section-title__title">feel free to get in<br> touch with me</h2>
+        </div><!-- section-title -->
+        <div class="contact-one__form-box  text-center">
+            <form action="https://bracketweb.com/nisoz-html/assets/inc/sendemail.php" class="contact-one__form contact-form-validated" novalidate="novalidate">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="contact-one__input-box">
+                            <input type="text" placeholder="Your name" name="name">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="contact-one__input-box">
+                            <input type="email" placeholder="Email address" name="email">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="contact-one__input-box">
+                            <input type="text" placeholder="Phone" name="phone">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="contact-one__input-box">
+                            <select class="selectpicker" aria-label="Default select example">
+                                <option selected>Select service</option>
+                                <option value="1">Select service 01</option>
+                                <option value="2">Select service 02</option>
+                                <option value="3">Select service 03</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="contact-one__input-box text-message-box">
+                            <textarea name="message" placeholder="Write Comment"></textarea>
+                        </div>
+                        <div class="contact-one__btn-box">
+                            <button type="submit" class="nisoz-btn">
+                                <span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span><span class="nisoz-btn__shape"></span>
+                                <span class="nisoz-btn__text">Send a message</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            <div class="result"></div>
+        </div>
+    </div>
+</section>
+
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('script'); ?>
+<script src="<?php echo e(asset('assets/vendors/countdown/bootstrap-select.min.js')); ?>"></script>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\dell\Desktop\html_Tamplate\laravel-projects\chiku-redesign\resources\views/contact.blade.php ENDPATH**/ ?>
